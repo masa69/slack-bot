084570c462e111f850c6053e5cc354d64503384b
masa69 <psychedelic.nekopunch@gmail.com>
2016-08-06 13:22:31 +0900

    google v3 api と weather map api のキー追加

--- config.sample.js
+++ config.sample.js
@@ -4,7 +4,11 @@
 
 	var self = {};
 
+	self.GOOGLE_APIS_KEY = 'YOUR GOOGLE_APIS_KEY';
+
 	self.SLACK_API_TOKEN = 'YOUR USER BOTs SLACK_API_TOKEN';
+
+	self.OPEN_WEATHER_MAP_API_KEY = 'YOUR OPEN_WEATHER_MAP_API_KEY';
 
 	self.peros = [];
 
